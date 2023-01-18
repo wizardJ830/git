5a5672c681e3a0ab8e736257f7648afe81a54bb6
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2023-01-18 17:09:15 +0100

    builtin/rebase.c: free() "options.strategy_opts"
    When the "strategy_opts" member was added in ba1905a5fef (builtin
    rebase: add support for custom merge strategies, 2018-09-04) the
    corresponding free() for it at the end of cmd_rebase() wasn't added,
    let's do so.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/rebase.c
+++ builtin/rebase.c
@@ -1835,6 +1835,7 @@ cleanup:
 	free(options.gpg_sign_opt);
 	free(options.cmd);
 	free(options.strategy);
+	free(options.strategy_opts);
 	strbuf_release(&options.git_format_patch_opt);
 	free(squash_onto_name);
 	free(keep_base_onto_name);
